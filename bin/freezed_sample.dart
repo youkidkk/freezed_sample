eb3123934daebf99793b6cb4375ae6a682971147
youkidkk <youkidkk80@gmail.com>
2022-01-14 23:23:34 +0900

    コメント追加

--- bin/freezed_sample.dart
+++ bin/freezed_sample.dart
@@ -4,9 +4,11 @@ import "basic/basic.dart";
 import "json/json_model.dart";
 
 void main(List<String> arguments) {
+  // 基本型
   final basic = Basic(number: 123, text: "Text");
   print(basic);
 
+  // JSON変換可能型
   final jsonModel = JsonModel.fromJson(
     json.decode(
       '''
